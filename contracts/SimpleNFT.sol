fbc77ececc740425a2e0fe7f2b92ec0fef980a08
a-colabella <acolabella11@gmail.com>
2022-10-30 20:26:42 -0600

    simple nft

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -6,36 +6,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NonProfit is ERC721, ERC721URIStorage, Ownable {
+contract SimpleNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("NonProfit", "NPRFT") {}
+    constructor() ERC721("Title", "TKN") {}
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function mint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 
     function withdraw() public payable onlyOwner {
